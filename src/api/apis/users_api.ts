d54978839482b6ca4e78109b97279d4cf1c6639a
nrser <neil@neilsouza.com>
2021-10-19 23:01:32 -0700

    Had the path wrong

--- src/api/apis/users_api.ts
+++ src/api/apis/users_api.ts
@@ -3,6 +3,6 @@ import BaseApi from "./base_api";
 
 export default class UsersApi extends BaseApi {
     me(): Promise<UserObject> {
-        return this._http.get<UserObject>("/me");
+        return this._http.get<UserObject>("/v1/me");
     }
 }
